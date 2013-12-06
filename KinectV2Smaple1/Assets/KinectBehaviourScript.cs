06514cd693843b91bc4a39a19d669eb19a33df59
Kaoru NAKAMURA <kaorun55@gmail.com>
2013-12-07 07:04:19 +0900

    Unityのサンプルを追加

---
+++ KinectV2Smaple1/Assets/KinectBehaviourScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using KinectSensor;
+
+public class KinectBehaviourScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		IntPtr sensor = IntPtr.Zero;
+		var hr = NativeMethods.GetDefaultKinectSensor( ref sensor );
+		print( "Kinect Initialize : " + hr.ToString() );
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
