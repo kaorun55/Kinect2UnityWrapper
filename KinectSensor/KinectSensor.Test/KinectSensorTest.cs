0a9f3f349af3699810a31fb0420b02fdc6ce452f
Kaoru NAKAMURA <kaorun55@gmail.com>
2013-12-07 06:57:46 +0900

    簡単なラッパーを追加

---
+++ KinectSensor/KinectSensor.Test/KinectSensorTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace KinectSensor.Test
+{
+    [TestClass]
+    public class KinectSensorTest
+    {
+        // V2
+
+        [TestMethod]
+        public void GetDefaultKinectSensor()
+        {
+            IntPtr sensor = IntPtr.Zero;
+            var hr = NativeMethods.GetDefaultKinectSensor( ref sensor );
+            Assert.AreNotEqual( 0, hr );
+        }
+
+        [TestMethod]
+        public void GetKinectSensorCollection()
+        {
+            IntPtr sensor = IntPtr.Zero;
+            var hr = NativeMethods.GetKinectSensorCollection( ref sensor );
+            Assert.AreNotEqual( 0, hr );
+        }
+
+        // V1
+
+        [TestMethod]
+        public void NuiCreateSensorByIndex()
+        {
+            int count = 0;
+            int hr = NativeMethods.NuiGetSensorCount( ref count );
+            Assert.AreEqual( 0, hr );
+        }
+    }
+}
---
+++ KinectSensor/KinectSensor/KinectSensor.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace KinectSensor
+{
+    public class NativeMethods
+    {
+        // V2
+        [DllImportAttribute( @"C:\Windows\System32\Kinect30.dll", EntryPoint = "GetKinectSensor" )]
+        public static extern int GetKinectSensorCollection( ref IntPtr kinectSensorCollection );
+
+        [DllImportAttribute( @"C:\Windows\System32\Kinect20.dll", EntryPoint = "GetDefaultKinectSensor" )]
+        public static extern int GetDefaultKinectSensor( ref IntPtr defaultKinectSensor );
+
+        // V1
+        //INuiSensor **ppNuiSensor
+        [DllImportAttribute( @"C:\Windows\System32\Kinect10.dll", EntryPoint = "NuiCreateSensorByIndex" )]
+        public static extern uint NuiCreateSensorByIndex( int index, ref IntPtr ppNuiSensor );
+
+        [DllImportAttribute( @"C:\Windows\System32\Kinect10.dll", EntryPoint = "NuiGetSensorCount" )]
+        public static extern int NuiGetSensorCount( ref int pCount );
+    }
+
+    [Guid( "3C6EBA94-0DE1-4360-B6D4-653A10794C8B" )]
+    [InterfaceType( ComInterfaceType.InterfaceIsIUnknown )]
+    [ComImport()]
+    public interface IKinectSensor
+    {
+        [MethodImpl( MethodImplOptions.InternalCall, MethodCodeType=MethodCodeType.Runtime )]
+        [PreserveSig]
+        uint get_Status( out IntPtr status );
+    }
+}
